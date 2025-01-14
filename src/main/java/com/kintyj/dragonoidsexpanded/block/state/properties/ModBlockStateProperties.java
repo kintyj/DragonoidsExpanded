68981d56ede897e47cd6750e047ce04c74ee50e8
Darthsae <70599537+Darthsae@users.noreply.github.com>
2025-01-13 19:23:03 -0500

    COMMISSION

---
+++ src/main/java/com/kintyj/dragonoidsexpanded/block/state/properties/ModBlockStateProperties.java
@@ -0,0 +1,7 @@
+package com.kintyj.dragonoidsexpanded.block.state.properties;
+
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+public class ModBlockStateProperties {
+    public static final IntegerProperty SLIME_STATE = IntegerProperty.create("slime_state", 0, 4);
+}
