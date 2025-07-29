ac5e6b5599417dc7c5d51b191a1301d9c15cc6e2
Darthsae <sockmit2007@gmail.com>
2025-07-28 21:16:27 -0400

    Goner

--- src/main/java/com/kintyj/dragonoidsexpanded/entity/Wyvern.java
+++ src/main/java/com/kintyj/dragonoidsexpanded/entity/Wyvern.java
@@ -223,7 +223,7 @@ public class Wyvern extends TamableAnimal
             if (timer <= 0) {
                 timer = getRandom().nextIntBetweenInclusive(yawnDelayMin, yawnDelayMax);
                 triggerAnim("attackController", "yawn");
-                playSound(DragonoidsExpanded.WYVERN_CALL.get(), 3.0f, 1.0f);
+                playSound(DragonoidsExpanded.WYVERN_CALL.get(), 0.25f, 1.0f);
 
             } else {
                 timer--;
